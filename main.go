4163d22ce5e88d2335bdf6154a149a4c42bc7af2
Larapriar <larapriar@gmail.com>
2021-02-23 21:07:15 +0100

    third way of defining structs

--- main.go
+++ main.go
@@ -8,10 +8,22 @@ type person struct {
 }
 
 func main() {
+
+	//first way
 	alex := person{"Alex", "Anderson"}
 	fmt.Println(alex.firstName)
+
+	//second way
 	miranda := person{firstName: "Miranda", lastName: "Anderson"}
 	fmt.Println(miranda.firstName)
 	fmt.Println(alex)
+	//prints--> {firstName:Alex lastName:Anderson}
+	fmt.Printf("%+v", alex)
+
+	//third way
+	var unai person
+	unai.firstName = "Unai"
+	unai.lastName = "Aanders"
+	fmt.Println(unai)
 
 }
